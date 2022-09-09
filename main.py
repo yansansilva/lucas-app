3272a4e63ac479b4298ba3c28aa722910d031f59
yansansilva <86764284+yansansilva@users.noreply.github.com>
2022-09-08 21:05:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -28,61 +28,77 @@ def converter_df_excel(df):
     return processed_data
 
 coluna_1, coluna_2, coluna_3 = st.columns((3, 3, 3))
-arquivo = coluna_1.file_uploader("Upload do Arquivo")#, accept_multiple_files=True)
+arquivos = st.file_uploader('Upload do(s) arquivo(s)', accept_multiple_files=True)
 
-lista = []
-resultado = pd.DataFrame()
+lista_arquivos = []
+lista_resultados = []
+espaco='\u0000'
+iteracao_resultado = 0
 
-if arquivo is not None:
-    espaco='\u0000'
-    dados_importados = pd.read_csv(arquivo, delimiter=f'No Liner{11*espaco}0.  Water{19*espaco}', encoding='ISO-8859-1', header=None)
-    if len(dados_importados) != 1:
-        dados_concatenados = pd.concat([dados_importados[1], dados_importados[0]], ignore_index=True).dropna().reset_index(drop=True).drop(labels=1).reset_index(drop=True).squeeze()#[0].split(',')
-        dados_em_lista = []
-        for x in range(len(dados_concatenados)):
-            dados_em_lista.append(dados_concatenados[x].split(','))
-        dados_transpostos = pd.DataFrame(dados_em_lista).transpose()
-        dados_concatenados_2 = pd.DataFrame()
-        for x in range(len(dados_transpostos.columns)):
-            dados_concatenados_2 = pd.concat([dados_concatenados_2, pd.DataFrame(dados_transpostos)[x]], ignore_index=True)
-        dados = dados_concatenados_2.dropna().reset_index(drop=True).squeeze().tolist()
-    else:
-        dados = dados_importados.squeeze()[1].split(',')
+for arquivo in arquivos:
+    if arquivo.name not in str(lista_arquivos):
+        lista_arquivos.append(arquivo)
+
+if lista_arquivos == []:
+    iteracao_resultado = 0
+
+if lista_arquivos != []:
+    for arquivo in lista_arquivos:
+        lista = []
+        resultado = pd.DataFrame()
+
+        dados_importados = pd.read_csv(arquivo, delimiter=f'No Liner{11*espaco}0.  Water{19*espaco}', encoding='ISO-8859-1', header=None)
+        if len(dados_importados) != 1:
+            dados_concatenados = pd.concat([dados_importados[1], dados_importados[0]], ignore_index=True).dropna().reset_index(drop=True).drop(labels=1).reset_index(drop=True).squeeze()#[0].split(',')
+            dados_em_lista = []
+            for x in range(len(dados_concatenados)):
+                dados_em_lista.append(dados_concatenados[x].split(','))
+            dados_transpostos = pd.DataFrame(dados_em_lista).transpose()
+            dados_concatenados_2 = pd.DataFrame()
+            for x in range(len(dados_transpostos.columns)):
+                dados_concatenados_2 = pd.concat([dados_concatenados_2, pd.DataFrame(dados_transpostos)[x]], ignore_index=True)
+            dados = dados_concatenados_2.dropna().reset_index(drop=True).squeeze().tolist()
+        else:
+            dados = dados_importados.squeeze()[1].split(',')
 
 
-    lista_dados = []
-    for linha, dado in enumerate(dados):
-        if len(dado) > 10:
-            if 0 < linha < len(dados) - 1:
-                if len(dados[linha + 1]) <= 10:
-                    lista_dados.append([f'{dados[linha - 1][-8:]},{dado[:-1]}'])
+        lista_dados = []
+        for linha, dado in enumerate(dados):
+            if len(dado) > 10:
+                if 0 < linha < len(dados) - 1:
+                    if len(dados[linha + 1]) <= 10:
+                        lista_dados.append([f'{dados[linha - 1][-8:]},{dado[:-1]}'])
+                    else:
+                        lista_dados.append([f'{dados[linha - 1][-8:]},{dado[:-9]}'])
                 else:
-                    lista_dados.append([f'{dados[linha - 1][-8:]},{dado[:-9]}'])
-            else:
-                lista_dados.append([f'{dados[linha - 1][-8:]},{dado[:-1]}'])
+                    lista_dados.append([f'{dados[linha - 1][-8:]},{dado[:-1]}'])
 
-    lista = [re.sub('\u0000', ' ', x.lstrip()) for x in pd.DataFrame(lista_dados).squeeze()]
+        lista = [re.sub('\u0000', ' ', x.lstrip()) for x in pd.DataFrame(lista_dados).squeeze()]
 
-    if lista != []:
-        separacao_lista = [[lista[x][0:17], float(lista[x][17:30]), float(lista[x][30:43]), float(lista[x][43:54]), float(lista[x][54:65]), float(lista[x][65:79])] for x in range(len(lista))]
-        resultado = pd.DataFrame(separacao_lista, columns=['data', 'flow', 'velocidade', 'net', 'positive', 'negative'])
-        mostrar = st.checkbox('Exibir Resultados')
-        if mostrar:
-            st.dataframe(resultado)
+        if lista != []:
+            separacao_lista = [[lista[x][0:17], float(lista[x][17:30]), float(lista[x][30:43]), float(lista[x][43:54]), float(lista[x][54:65]), float(lista[x][65:79])] for x in range(len(lista))]
+            resultado = pd.DataFrame(separacao_lista, columns=['data', 'flow', 'velocidade', 'net', 'positive', 'negative'])
+            lista_resultados.append(resultado)
+            mostrar = st.checkbox(f'Exibir Resultados do arquivo: {lista_arquivos[iteracao_resultado].name}')
+            if mostrar:
+                st.dataframe(lista_resultados[iteracao_resultado])
 
-if not resultado.empty:
-    data = [resultado['data'][resultado.index[0]][:8], resultado['data'][resultado.index[-1]][:8]]
+        iteracao_resultado = iteracao_resultado + 1
 
-    st.write("### Salvar Resultados")
+if lista_resultados != []:
+    for x in range(len(lista_resultados)):
+        data = [lista_resultados[x]['data'][resultado.index[0]][:8], resultado['data'][resultado.index[-1]][:8]]
 
-    coluna_nomear_arquivo_1, coluna_nomear_arquivo_2 = st.columns((3, 2))
-    if data[0] != data[1]:
-        nomearquivo = coluna_nomear_arquivo_1.text_input('Digite um nome para o arquivo:', f'medição_de_{data[0]}_a_{data[1]}')
-    else:
-        nomearquivo = coluna_nomear_arquivo_1.text_input('Digite um nome para o arquivo:', f'medição_de_{data[0]}')
+        st.write("### Salvar Resultados")
 
-    coluna_salvar_1, coluna_salvar_2, coluna_salvar_3 = st.columns((2, 2, 6))
-    csv = converter_df_csv(resultado)
-    excel = converter_df_excel(resultado)
-    coluna_salvar_1.download_button(label="Download em CSV", data=csv, file_name=nomearquivo + '.csv', mime='text/csv')
-    coluna_salvar_2.download_button(label="Download em Excel", data=excel, file_name=nomearquivo+'.xlsx', mime='application/vnd.ms-excel')
+        coluna_nomear_arquivo_1, coluna_nomear_arquivo_2 = st.columns((3, 2))
+        if data[0] != data[1]:
+            nomearquivo = coluna_nomear_arquivo_1.text_input('Digite um nome para o arquivo:', f'{lista_arquivos[x].name}_medição_de_{data[0]}_a_{data[1]}')
+        else:
+            nomearquivo = coluna_nomear_arquivo_1.text_input('Digite um nome para o arquivo:', f'{lista_arquivos[x].name}_medição_de_{data[0]}')
+
+        coluna_salvar_1, coluna_salvar_2, coluna_salvar_3 = st.columns((2, 2, 6))
+        csv = converter_df_csv(lista_resultados[x])
+        excel = converter_df_excel(lista_resultados[x])
+        coluna_salvar_1.download_button(label="Download em CSV", data=csv, file_name=nomearquivo + '.csv', mime='text/csv')
+        coluna_salvar_2.download_button(label="Download em Excel", data=excel, file_name=nomearquivo+'.xlsx', mime='application/vnd.ms-excel')
